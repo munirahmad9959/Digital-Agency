dbdc7165054dcb1f829dd50a37c1064cf0c8f3a5
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-08-04 23:44:23 +0500

    Working on HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,26 @@
+import styles from "./home.module.css"
+import Image from "next/image";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, ratione id. Eos possimus at nam cupiditate quisquam.</p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src='/brands.png' alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+
+    </div>
+  )
 };
 
 export default Home;
